a1bf00a8bb149c575df23847d316034a70b95855
Thales <thalesalgusto401@gmail.com>
2019-04-26 09:15:58 -0300

    Gerador de Nomes Pokemon PRONTO E TESTADO!

--- src/Trabalho/GeraNomes.java
+++ src/Trabalho/GeraNomes.java
@@ -5,12 +5,17 @@ abstract class GeraNomes {
 		String[] Iniciais = 		{"Pi",	"Pu",	"Amu",	"Me",	"Ga",	"Ska",	"Sno",	"Mu",	"Me",	"Aka",	"Shi",	"Ine",	"Ba",	"Ca",	"Sno",	"Mu",	"Cha",	"Squi",	"War",	"Wee",	"Pi",	"Sand",	"Bee"}; //tem 23
 		String[] Intermediarias = 	{"pli",	"sa",	"ka",	"ku",	"na",	"ti",	"ca",	"te",	"be",	"ma",	"pli",	"gli",	"na",	"su"}; //tem 14
 		String[] Finais = 			{"king","dgey",	"shrew","kans",	"row",	"drill","queen","fairy","pix",	"puff",	"bat",	"oom",	"sect",	"moth"};//tem 14
-		int Tamanho = (int)(Math.random()*3 + 2 );
-		String nome=Iniciais[(int)Math.random()*23];
+		int Tamanho = (int)(Math.random()*2 + 2 );
+		int index = (int)(Math.random()*23);
+		System.out.println(index);
+		String nome=Iniciais[index];
+		
 		while ( (Tamanho--)>1 ) {
-			nome+=Intermediarias[(int)Math.random()*14];
+			index = (int)(Math.random()*14);
+			nome=nome + Intermediarias[index];
 		}
-		nome+=Finais[(int)Math.random()*14];
+		index = (int)(Math.random()*14);
+		nome+=Finais[index];
 		return nome;
 	}
 }
---
+++ src/Trabalho/TesteNomes.java
@@ -0,0 +1,8 @@
+package Trabalho;
+
+public class TesteNomes {
+	public static void main(String[] args) {
+		int n=50;
+		while (n-- >1)System.out.println(GeraNomes.NomePokemon());
+	}
+}
