d76f269ee9ff58456a96ad15b1851a53604bc57a
Thales <thalesalgusto401@gmail.com>
2019-04-26 09:18:00 -0300

    Pequeno ajuste nomes

--- src/Trabalho/GeraNomes.java
+++ src/Trabalho/GeraNomes.java
@@ -5,7 +5,7 @@ abstract class GeraNomes {
 		String[] Iniciais = 		{"Pi",	"Pu",	"Amu",	"Me",	"Ga",	"Ska",	"Sno",	"Mu",	"Me",	"Aka",	"Shi",	"Ine",	"Ba",	"Ca",	"Sno",	"Mu",	"Cha",	"Squi",	"War",	"Wee",	"Pi",	"Sand",	"Bee"}; //tem 23
 		String[] Intermediarias = 	{"pli",	"sa",	"ka",	"ku",	"na",	"ti",	"ca",	"te",	"be",	"ma",	"pli",	"gli",	"na",	"su"}; //tem 14
 		String[] Finais = 			{"king","dgey",	"shrew","kans",	"row",	"drill","queen","fairy","pix",	"puff",	"bat",	"oom",	"sect",	"moth"};//tem 14
-		int Tamanho = (int)(Math.random()*2 + 2 );
+		int Tamanho = (int)(Math.random()*2 + 1 );
 		int index = (int)(Math.random()*23);
 		System.out.println(index);
 		String nome=Iniciais[index];
