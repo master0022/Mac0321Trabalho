99bfffb10c174e97b1d8f3407324cb008a94abd5
Thales <Thalesasr@hotmail.com>
2019-05-16 18:14:34 -0300

    TesteGeral foi atualizado para incluir informacoes mega importantes sobre o treinador

--- src/Trabalho/TesteGeral.java
+++ src/Trabalho/TesteGeral.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class TesteGeral {
 
 	public static void main(String[] args) {
+		
 		Scanner Input = new Scanner(System.in);
-		int n = 5;
-		Treinador eu = new Treinador(4,"Thales",false);
+		Treinador eu = new Treinador(4,"Monitor muito legal",false);
+		
 		Mapa a = Mapa.CriaMapa();
 
 		eu.SetPlayer();
